9447e0ef22cd26435c70965cab9dce92ce6076fb
Sean <sean.ryan.martin@gmail.com>
2020-12-16 17:00:31 -0500

    baseline

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/ui/Header'
+import {ThemeProvider} from '@material-ui/styles'
+import theme from './ui/Theme'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,12 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+
+export default createMuiTheme({
+    typography:{
+        tab:{
+            fontSize: "1.2rem",
+            minWidth: 15,
+            marginLeft: "25px"
+        }
+    }
+
+})
