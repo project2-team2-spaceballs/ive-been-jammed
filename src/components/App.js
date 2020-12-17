3ccd5df3ecb92a1c55e19b5d4f10af476c0765f2
fallensteed <scott@hubertart.com>
2020-12-17 08:04:29 -0700

    First hacks.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Header from '../components/ui/Header'
-import {ThemeProvider} from '@material-ui/styles'
-import theme from './ui/Theme'
+import React from "react";
+import Header from "../components/ui/Header";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./ui/Theme";
+import Logs from './Logs';
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Header/>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Header />
+            <Logs />
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Logs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Logs = () => {
+    return <h2>Logs</h2>;
+};
+
+export default Logs;
