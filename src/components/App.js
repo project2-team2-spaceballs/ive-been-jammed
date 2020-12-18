d8990f95acd382b20414438bd352197d3ab660e7
Sean Martin <sean.ryan.martin@gmail.com>
2020-12-18 16:12:20 -0500

    operational

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import Mx from './Mx/Mx'
 import Logs from './logs/Logs'
 import AssetRequest from './assetRequest/AssetRequest'
 import {Route,Switch, BrowserRouter} from 'react-router-dom'
+import Opscap from './opscap/Opscap'
+import PosNeg from './posneg/posneg'
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
       <Route path='/mx'><Mx/></Route>
       <Route path='/logs'><Logs/></Route>
       <Route path='/assetrequest'><AssetRequest/></Route>
+      <Route path='/opscap'><Opscap/></Route>
+      <Route path='/posneg'><PosNeg/></Route>
       </Switch>
       </BrowserRouter>
     </ThemeProvider>
