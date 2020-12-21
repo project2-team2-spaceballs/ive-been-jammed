ce968e96ac164e26a330e7b4d5d8e5d3840d1768
Sean Martin <sean.ryan.martin@gmail.com>
2020-12-21 10:08:23 -0500

    weekend changes

--- src/components/Mx/Mx.jsx
+++ src/components/Mx/Mx.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import MxProvider from './MxProvider'
+import MxTasks from './MxTasks'
 import SensorSelect from './SensorSelect'
+import SubmitRequest from './SubmitRequest'
 
 export default function Mx(){
     return(
         <MxProvider>
             <SensorSelect/>
+            <MxTasks/>
+            <SubmitRequest/>
         </MxProvider>
     )
 }
